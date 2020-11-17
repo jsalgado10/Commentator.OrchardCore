421a6cbac08f5645003d133000e4e44daf12da7b
jsalgadodcr <jsalgado@dcrpos.com>
2020-11-17 13:13:50 -0600

    change manifest file

--- Manifest.cs
+++ Manifest.cs
@@ -2,8 +2,8 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "Commentator",
-    Author = "DCR",
-    Website = "https://dcrpos.com",
+    Author = "JFD Service",
+    Website = "https://jfdservice.com",
     Version = "1.0.0",
     Description = "The Commentator module allows you to add a comments section to your Content Type.",
     Category = "Comments"
